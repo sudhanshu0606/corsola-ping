b450e3a2a68a77841eb9af05261512a10eef044b
Sudhanshu <125023291+sudhanshu0606@users.noreply.github.com>
2025-05-14 10:52:11 +0530

    Update route.ts

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,8 +1,5 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    return NextResponse.json(
-        { message: "Pong" },
-        { status: 200 }
-    );
+export async function HEAD() {
+    return NextResponse.json({}, { status: 200 });
 }
